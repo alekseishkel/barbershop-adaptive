bd81e5a26db416304de12a3e653ab4a65248dcc0
Alex <lexashkel@mal.ru>
2019-05-16 17:28:02 +0300

    полная верстка барбершопа без ретины

--- script/script.js
+++ script/script.js
@@ -1,5 +1,8 @@
 var navMain = document.querySelector('.nav-main');
 var navToggle = document.querySelector('.nav-main__toggle');
+var link = document.querySelector(".user-list__login");
+var popup = document.querySelector(".personal-account");
+var close = popup.querySelector(".modal__close");
 
 navMain.classList.remove('nav-main--no-js');
 
@@ -11,4 +14,15 @@ navToggle.addEventListener('click', function () {
         navMain.classList.add('nav-main--closed');
         navMain.classList.remove('nav-main--opened');
     }
+});
+
+link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("modal-show");
+    login.focus();
+});
+
+close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
 });
